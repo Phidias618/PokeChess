cba38c4cb86f00a6dee53ec64972de359b0df0d3
Eliott <Eliott@PC-NICOLAS>
2025-03-22 22:39:25 +0100

    Quick claw added

--- Build x64/assets/convert.py
+++ Build x64/assets/convert.py
@@ -1,4 +1,4 @@
-tab: list[list[int]] = [[0 for _ in range(41)] for _ in range(19)]
+tab: list[list[int]] = [[0 for _ in range(40)] for _ in range(18)]
 
 with open("ability.txt", 'r') as inp:
     for line in inp:
@@ -9,12 +9,14 @@ with open("ability.txt", 'r') as inp:
         y, x = tuple(map(int, yx.split(', ')))
         val = int(val[3:], 16)
 
-        tab[y][x] = val
+        tab[y-1][x-1] = val
+
 print(*tab, sep='\n')
-with open("ability_", 'w') as out:
+with open("ability", 'wb') as out:
     for y, row in enumerate(tab):
         for x, val in enumerate(row):
-
-            h = ''.join(map(lambda c: chr(int(c, 16)) ,('0' * 16 + hex(val)[2:])[-16:]))[::2]
-
+            padded_hex = ('0' * 16 + hex(val)[2:])[-16:]
+            h = bytearray(map(lambda c: int(c, 16), (padded_hex[2*i:2*i+2] for i in range(8))))
+            print(list(map(lambda c: int(c, 16), (padded_hex[2*i:2*i+2] for i in range(8)))))
+            
             out.write(h)
